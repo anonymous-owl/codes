7a7641e633605c65dcf88e357bfb4187ff64ae84
anonymous-owl <fairregression@gmail.com>
2024-05-22 18:52:00 +0200

    updated plots

--- plots.py
+++ plots.py
@@ -1,5 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+
+
+def plot_kde_compare(fair_model, base_model, X, S, B,
+                     linewidth=1.5, figsize=(14,5), fontsize=10):
+    
+    plt.figure(figsize=figsize, dpi=200)
+    
+    S_val = sorted(S.unique())
+    
+    y_pred_fair = fair_model.predict(X)
+    y_pred_base = base_model.predict(X)
+    
+    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(15,4))
+
+    ax1.set_title("Fair model", fontsize=fontsize)
+    for s in S_val:
+        kde = stats.gaussian_kde(y_pred_fair[S == s])
+        xx = np.linspace(-B, B, 1000)
+        ax1.plot(xx, kde(xx), label='s='+str(s), alpha=0.5,)
+    ax1.legend()
+        
+    ax2.set_title("Base model", fontsize=fontsize)
+    for s in S_val:
+        kde = stats.gaussian_kde(y_pred_base[S == s])
+        xx = np.linspace(-B, B, 1000)
+        ax2.plot(xx, kde(xx), label='s='+str(s), alpha=0.5,)
+    ax2.legend()
+    
+    plt.show()
 
 def plot_distributions_compare(fair_model, base_model, X, S):
     
@@ -75,23 +105,24 @@ def moving_average(x, w):
 
 #plot unfairness history wrt number of iterations (takes the history from FairReg.history)
 def plot_risk_history(risk_history_all, T, moving_av=1, dataset='law school', 
-                      alpha=1, color='r', x_label = 'num of iterations', y_label='risk'):
-   
+                      alpha=1, color='r', x_label = 'num of iterations', y_label='risk',
+                      figsize=(14,5), fontsize=10):
     
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(dataset)
-    
+    plt.figure(figsize=figsize, dpi=200)
+    plt.title(dataset, fontsize=fontsize)
+    plt.xlabel(x_label, fontsize=fontsize)
+    plt.ylabel(y_label, fontsize=fontsize)
+
     risk_history = np.mean(risk_history_all, axis=0)
     risk_std = np.std(risk_history_all, axis=0)
     
     if moving_av==1:
         iter_ = np.arange(1, len(risk_history)+1)*(T/len(risk_history))
-        plt.plot(iter_, risk_history, alpha=alpha, label="risk", c='r')
+        plt.plot(iter_, risk_history, alpha=alpha, label=r'$risk$', c='r')
         plt.fill_between(iter_, risk_history-risk_std,risk_history+risk_std, alpha=.2, color=color, lw=0)
     elif moving_av>1:
         iter_ma = np.arange(moving_av, len(risk_history)+1)*(T/len(risk_history))
-        plt.plot(iter_ma, moving_average(risk_history, moving_av), alpha=alpha, label="risk - MA", c=color)
+        plt.plot(iter_ma, moving_average(risk_history, moving_av), alpha=alpha, label=r'$risk - MA$', c=color)
         plt.fill_between(iter_ma, moving_average(risk_history-risk_std, moving_av),
                          moving_average(risk_history+risk_std, moving_av), alpha=.2, color=color, lw=0)
         
@@ -99,11 +130,13 @@ def plot_risk_history(risk_history_all, T, moving_av=1, dataset='law school',
     
 #plot unfairness history wrt number of iterations (takes the history from FairReg.history)
 def plot_unfairness_history(unf_all, T, K, moving_av=1, dataset='law school', 
-                            alpha=1, colors = ['g', 'orange'], x_label='num of iterations', y_label='unfairness'):
+                            alpha=1, colors = ['g', 'orange'], x_label='num of iterations', y_label='unfairness',
+                            figsize=(14,5), fontsize=10):
     
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(dataset)
+    plt.figure(figsize=figsize, dpi=200)
+    plt.title(dataset, fontsize=fontsize)
+    plt.xlabel(x_label, fontsize=fontsize)
+    plt.ylabel(y_label, fontsize=fontsize)
       
     for s in range(K):
         
@@ -112,13 +145,13 @@ def plot_unfairness_history(unf_all, T, K, moving_av=1, dataset='law school',
         
         if moving_av==1:
             iter_ = np.arange(1, len(unf_history)+1)*(T/len(unf_history))
-            plt.plot(iter_, unf_history, alpha=alpha, label="unf : S="+str(s), c=colors[s])
+            plt.plot(iter_, unf_history, alpha=alpha, label=r'$unf : S =$' +str(s), c=colors[s])
             plt.fill_between(iter_, unf_history-unf_std,unf_history+unf_std, 
                              alpha=.2, color=colors[s], lw=0)
         elif moving_av>1:
             iter_ma = np.arange(moving_av, len(unf_history)+1)*(T/len(unf_history))
             plt.plot(iter_ma, moving_average(unf_history, moving_av),
-                     alpha=alpha, label="unf - MA : S="+str(s),c=colors[s])
+                     alpha=alpha, label=r'$unf - MA : S =$' +str(s),c=colors[s])
             plt.fill_between(iter_ma, moving_average(unf_history-unf_std, moving_av),
                              moving_average(unf_history+unf_std, moving_av), 
                              alpha=.2, color=colors[s], lw=0)
@@ -165,14 +198,18 @@ def plot_risk_unf_compare(pairs_list, model_list, unf_type_list, risk_type_list,
                           markers_list=['o','s','x'], dataset='communities and crime', 
                           x_label = 'unfairness', y_label = 'risk',
                           S_list=[0], colors = [['tab:green'],['tab:orange']], legend_size=8, alpha=0.7, 
-                          plot_std=True, annotate=False, loglog=False, start_0=False, label='short', linestyle='dashed'):
+                          plot_std=True, annotate=False, loglog=False, start_0=False, label='short', linestyle='dashed', 
+                          figsize=(14,5), fontsize=10):
+    
+    
+    plt.figure(figsize=figsize, dpi=200)
+    plt.title(dataset, fontsize=fontsize)
+    plt.xlabel(x_label, fontsize=fontsize)
+    plt.ylabel(y_label, fontsize=fontsize)
+    
     if loglog:
         plt.xscale('log')
         plt.yscale('log')
-    
-    plt.title(dataset)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
     
     for i, pair in enumerate(pairs_list):
         unf, risk = pair[0], pair[1]
@@ -204,7 +241,7 @@ def plot_risk_unf_compare(pairs_list, model_list, unf_type_list, risk_type_list,
                     bar.set_alpha(0.15)
                 
             if annotate:
-                if model_list[i] != 'base':
+                if model_list[i] != '$base$':
                     for j in range(len(risk_)):
                         plt.annotate('eps'+str(j+1), (unf_[j], risk_[j]), fontsize=8)
             
